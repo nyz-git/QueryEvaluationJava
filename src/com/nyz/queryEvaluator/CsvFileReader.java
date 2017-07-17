c2b2e3fcff6741d448b976b4585e959b533837b2
Niyaz <nyz.git@gmail.com>
2017-07-17 18:01:14 +0530

    method for fetching header from csv done

--- src/com/nyz/queryEvaluator/CsvFileReader.java
+++ src/com/nyz/queryEvaluator/CsvFileReader.java
@@ -1,5 +1,22 @@
 package com.nyz.queryEvaluator;
 
-public class CsvFileReader {
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
+public class CsvFileReader {
+	private BufferedReader bufferedReader;
+	String line = "";
+	private String[] headerColumn;
+
+	public String[] fetchHeader(String tableName) {
+		try {
+			bufferedReader = new BufferedReader(new FileReader("D:\\"+tableName+".csv"));
+			line = bufferedReader.readLine();
+			headerColumn = line.split(",");
+		} catch (IOException e) {
+			System.out.println("Fetching Header Failed");
+		}
+		return headerColumn;
+	}
 }
--- src/com/nyz/queryEvaluator/QueryEvaluationEngine.java
+++ src/com/nyz/queryEvaluator/QueryEvaluationEngine.java
@@ -5,7 +5,6 @@ public class QueryEvaluationEngine {
 	public void executeQuery(String query) {
 		// TODO Auto-generated method stub
 		
-		System.out.println(query);
 	}
 
 }
