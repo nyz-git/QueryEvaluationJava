9f40b35f489e7ca6de3f479cfa730b75172537f1
Niyaz <nyz.git@gmail.com>
2017-07-17 15:16:15 +0530

    Query taken from User

---
+++ src/com/nyz/queryEvaluator/QueryEvaluation.java
@@ -0,0 +1,16 @@
+package com.nyz.queryEvaluator;
+
+import java.util.Scanner;
+
+public class QueryEvaluation {
+
+	private static Scanner sc;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String query;
+		sc = new Scanner(System.in);
+		query = sc.nextLine();
+	}
+
+}
